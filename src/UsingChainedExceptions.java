236e28a22c3996c4d20de3eb3a9484dfe2190737
Ali <learncomputerswithali@gmail.com>
2020-05-05 22:11:02 +0200

    Initial commit showing chained exceptions

---
+++ src/UsingChainedExceptions.java
@@ -0,0 +1,37 @@
+
+public class UsingChainedExceptions {
+
+	public static void main(String[] args) {
+		try {
+			method1();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void method1() throws Exception {
+		try {
+			method2();
+		}
+		catch (Exception e) {
+			throw new Exception ("Exception thrown in method 1", e);
+		}
+	}
+	
+	public static void method2() throws Exception {
+		try {
+			method3();
+		}
+		catch (Exception e) {
+			throw new Exception ("Exception thrown in method 2", e);
+		}
+	}
+	
+	public static void method3() throws Exception {
+		throw new Exception ("Exception thrown in method 3");
+	}
+	
+	
+	
+}
